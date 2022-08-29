444bf66e4a49dd6169cc864376e8be4d608aae6e
blackwanjii <rakellensley@gmail.com>
2022-08-29 11:15:15 +0300

    first android commit

--- app/src/main/java/com/example/app1/IntroActivity.kt
+++ app/src/main/java/com/example/app1/IntroActivity.kt
@@ -1,12 +1,20 @@
 package com.example.app1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.foodapp.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000    )
+        }
     }
 }
